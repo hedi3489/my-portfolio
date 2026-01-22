64de6631a7ed801ff6865a5d4c85c6174b3301f2
RetiredHunter <belhassinehedi308@gmail.com>
2026-01-21 20:35:30 -0500

    Projects update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Projects from './Projects'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,24 +9,9 @@ function App() {
     <>
       <h1>Hedi Belhassine</h1>
       
+      <Projects />
 
-      <div>
-        <h2>What I built</h2>
-
-        <h3>2024 Paris Olympics API</h3>
-        <p>A REST-based API for the 2024 Paris Olympics resource, a tool providing
-          access to various resources (athletes, coaches, venues…) with thorough 
-          pagination, robust validation, & exception handling.</p>
-        
-        <br />
-
-        <h3>IoT Smart-Home Simulation</h3>
-        <p>A smart-home sim. that runs on a RPi. Using a breadboard & various 
-          components it simulates a home environment, and using an online dashboard,
-          the user can remotely monitor & the control the state of its components. </p>
       
-      </div>
-
     </>
   )
 }
