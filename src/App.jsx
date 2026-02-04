f2e3189161be5b0a379d566e6cb993d5d929b903
RetiredHunter <belhassinehedi308@gmail.com>
2026-02-04 15:13:42 -0500

    Initial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { MainContent, SideBar } from './Containers'
 import './Cards.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className='flex container'>
-        <MainContent />
-        <SideBar />
-      </div>
-    </>
+    <Routes>
+      {/* Home */}
+      <Route
+        path="/"
+        element={
+          <div className="flex container">
+            <MainContent />
+            <SideBar />
+          </div>
+        }
+      />
+
+      {/* Dummy project route */}
+      <Route
+        path="/projects/test"
+        element={
+          <div className="flex container">
+            <h1>Test Project Page</h1>
+          </div>
+        }
+      />
+    </Routes>
   )
 }
 
